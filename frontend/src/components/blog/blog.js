5a8669de0465ee1b93c66bb3ef248fe4b8b14b9d
LifeAsDev <angelocraft551@gmail.com>
2024-02-29 12:12:08 -0400

    blog main top

--- frontend/src/components/blog/blog.js
+++ frontend/src/components/blog/blog.js
@@ -3,23 +3,181 @@ import Image from "next/image";
 export default function Blog() {
   return (
     <main className={styles.main}>
-      <h2 className="blue">Recently shipped</h2>
-      <p>
-        We’re releasing new updates nearly every week. Stay on top of them here
-        with all our latest company news and views.
-      </p>
-      <div className={styles.entry}>
-        <Image
-          width={1390}
-          height={486}
-          alt="blog hero"
-          src="/images/blog/blog hero.png"
-        />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor.
-        </p>
-      </div>
+      <section className={styles.mainSection}>
+        <div className={styles.top}>
+          <div className={styles.entryHero}>
+            <Image
+              width={1390}
+              height={486}
+              alt="blog hero"
+              src="/images/blog/blog hero.png"
+            />
+            <h3>
+              Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit, Sed Do.
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore. Lorem Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat.
+            </p>
+            <div className={styles.entryHeroInfo}>
+              <p>By The Author</p>
+              <p>August 20, 2022</p>
+            </div>
+          </div>
+          <div className={styles.recentPosts}>
+            <h3>Recent Posts</h3>
+            <div className={`${styles.gridRecentPosts} scrollbar1`}>
+              <div className={styles.recentPostEntry}>
+                <Image
+                  width={500}
+                  height={500}
+                  src="/images/blog/entry.png"
+                  alt="entry"
+                ></Image>
+                <div className={styles.recentPostText}>
+                  <p className={styles.recentPostTitle}>
+                    Lorem Ipsum Dolor Sit Amet, Ipsum Adipiscing Elit, Sed Do,
+                    Ipsum Dolor Sit Amet Lorem Lipsum.
+                  </p>
+                  <div className={styles.recentPostEntryInfo}>
+                    <p className={styles.recentPostAuthor}>By Tracey Wilson</p>
+                    <p className={styles.recentPostDate}>August 20, 2022</p>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.recentPostEntry}>
+                <Image
+                  width={500}
+                  height={500}
+                  src="/images/blog/entry.png"
+                  alt="entry"
+                ></Image>
+                <div className={styles.recentPostText}>
+                  <p className={styles.recentPostTitle}>
+                    Lorem Ipsum Dolor Sit Amet, Ipsum Adipiscing Elit, Sed Do,
+                    Ipsum Dolor Sit Amet Lorem Lipsum.
+                  </p>
+                  <div className={styles.recentPostEntryInfo}>
+                    <p className={styles.recentPostAuthor}>By Tracey Wilson</p>
+                    <p className={styles.recentPostDate}>August 20, 2022</p>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.recentPostEntry}>
+                <Image
+                  width={500}
+                  height={500}
+                  src="/images/blog/entry.png"
+                  alt="entry"
+                ></Image>
+                <div className={styles.recentPostText}>
+                  <p className={styles.recentPostTitle}>
+                    Lorem Ipsum Dolor Sit Amet, Ipsum Adipiscing Elit, Sed Do,
+                    Ipsum Dolor Sit Amet Lorem Lipsum.
+                  </p>
+                  <div className={styles.recentPostEntryInfo}>
+                    <p className={styles.recentPostAuthor}>By Tracey Wilson</p>
+                    <p className={styles.recentPostDate}>August 20, 2022</p>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.recentPostEntry}>
+                <Image
+                  width={500}
+                  height={500}
+                  src="/images/blog/entry.png"
+                  alt="entry"
+                ></Image>
+                <div className={styles.recentPostText}>
+                  <p className={styles.recentPostTitle}>
+                    Lorem Ipsum Dolor Sit Amet, Ipsum Adipiscing Elit, Sed Do,
+                    Ipsum Dolor Sit Amet Lorem Lipsum.
+                  </p>
+                  <div className={styles.recentPostEntryInfo}>
+                    <p className={styles.recentPostAuthor}>By Tracey Wilson</p>
+                    <p className={styles.recentPostDate}>August 20, 2022</p>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.recentPostEntry}>
+                <Image
+                  width={500}
+                  height={500}
+                  src="/images/blog/entry.png"
+                  alt="entry"
+                ></Image>
+                <div className={styles.recentPostText}>
+                  <p className={styles.recentPostTitle}>
+                    Lorem Ipsum Dolor Sit Amet, Ipsum Adipiscing Elit, Sed Do,
+                    Ipsum Dolor Sit Amet Lorem Lipsum.
+                  </p>
+                  <div className={styles.recentPostEntryInfo}>
+                    <p className={styles.recentPostAuthor}>By Tracey Wilson</p>
+                    <p className={styles.recentPostDate}>August 20, 2022</p>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.recentPostEntry}>
+                <Image
+                  width={500}
+                  height={500}
+                  src="/images/blog/entry.png"
+                  alt="entry"
+                ></Image>
+                <div className={styles.recentPostText}>
+                  <p className={styles.recentPostTitle}>
+                    Lorem Ipsum Dolor Sit Amet, Ipsum Adipiscing Elit, Sed Do,
+                    Ipsum Dolor Sit Amet Lorem Lipsum.
+                  </p>
+                  <div className={styles.recentPostEntryInfo}>
+                    <p className={styles.recentPostAuthor}>By Tracey Wilson</p>
+                    <p className={styles.recentPostDate}>August 20, 2022</p>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.recentPostEntry}>
+                <Image
+                  width={500}
+                  height={500}
+                  src="/images/blog/entry.png"
+                  alt="entry"
+                ></Image>
+                <div className={styles.recentPostText}>
+                  <p className={styles.recentPostTitle}>
+                    Lorem Ipsum Dolor Sit Amet, Ipsum Adipiscing Elit, Sed Do,
+                    Ipsum Dolor Sit Amet Lorem Lipsum.
+                  </p>
+                  <div className={styles.recentPostEntryInfo}>
+                    <p className={styles.recentPostAuthor}>By Tracey Wilson</p>
+                    <p className={styles.recentPostDate}>August 20, 2022</p>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.recentPostEntry}>
+                <Image
+                  width={500}
+                  height={500}
+                  src="/images/blog/entry.png"
+                  alt="entry"
+                ></Image>
+                <div className={styles.recentPostText}>
+                  <p className={styles.recentPostTitle}>
+                    Lorem Ipsum Dolor Sit Amet, Ipsum Adipiscing Elit, Sed Do,
+                    Ipsum Dolor Sit Amet Lorem Lipsum.
+                  </p>
+                  <div className={styles.recentPostEntryInfo}>
+                    <p className={styles.recentPostAuthor}>By Tracey Wilson</p>
+                    <p className={styles.recentPostDate}>August 20, 2022</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <aside></aside>
       <div className={styles.entryGrid}>
         <div className={styles.entry}>
           <Image
