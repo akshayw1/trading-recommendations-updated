6c6a4d046da4acd2c9f31ac638f6a83530507f5f
LifeAsDev <angelocraft551@gmail.com>
2024-03-01 09:41:44 -0400

    blog hot stories section

--- frontend/src/components/blog/blog.js
+++ frontend/src/components/blog/blog.js
@@ -332,7 +332,32 @@ export default function Blog() {
           <div className={styles.entryGridShowMore}>Load More</div>
         </div>
       </section>
-      <aside></aside>
+      <aside className={styles.hotStory}>
+        <h3>Hot Stories</h3>
+        <div className={styles.hotStoryGrid}>
+          <div className={styles.hotStoryLabel}>#Bitcoin</div>
+          <div className={styles.hotStoryLabel}>#Stock-Market</div>
+          <div className={styles.hotStoryLabel}>#Trading</div>
+          <div className={styles.hotStoryLabel}>#ETF</div>
+          <div className={styles.hotStoryLabel}>#Ethereum</div>
+          <div className={styles.hotStoryLabel}>#Lorem</div>
+          <div className={styles.hotStoryLabel}>#Ipsum</div>
+          <div className={styles.hotStoryLabel}>#Bitcoin</div>
+          <div className={styles.hotStoryLabel}>#Stock-Market</div>
+          <div className={styles.hotStoryLabel}>#Trading</div>
+          <div className={styles.hotStoryLabel}>#ETF</div>
+          <div className={styles.hotStoryLabel}>#Ethereum</div>
+          <div className={styles.hotStoryLabel}>#Lorem</div>
+          <div className={styles.hotStoryLabel}>#Ipsum</div>{" "}
+          <div className={styles.hotStoryLabel}>#Bitcoin</div>
+          <div className={styles.hotStoryLabel}>#Stock-Market</div>
+          <div className={styles.hotStoryLabel}>#Trading</div>
+          <div className={styles.hotStoryLabel}>#ETF</div>
+          <div className={styles.hotStoryLabel}>#Ethereum</div>
+          <div className={styles.hotStoryLabel}>#Lorem</div>
+          <div className={styles.hotStoryLabel}>#Ipsum</div>
+        </div>
+      </aside>
     </main>
   );
 }
